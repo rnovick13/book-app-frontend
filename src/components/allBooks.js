4ea3b20a4883102954c5a77b32eca8a1ff45b246
rnovick13 <rnovick13@gmail.com>
2019-06-19 14:17:07 -0400

    started building components

---
+++ src/components/allBooks.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import Book from './book'
+
+class AllBook extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+
+export default connect()(AllBook)
---
+++ src/components/book.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class Book extends Component {
+
+}
+
+export default connect()(Book);
