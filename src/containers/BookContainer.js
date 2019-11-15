628237165e02136ec235b21bf0d269a160707508
rnovick13 <rnovick13@gmail.com>
2019-11-15 13:14:36 -0500

    fetch favorites work

--- src/containers/BookContainer.js
+++ src/containers/BookContainer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import BookCard from '../components/BookCard'
 // import books from '../book-data'
 const API = `http://localhost:3000/books`
+const API2 = `http://localhost:3000/favorites`
 
 class BookContainer extends Component {
   constructor() {
@@ -16,6 +17,9 @@ class BookContainer extends Component {
     fetch(API)
       .then(response => response.json())
       .then(books => this.setState({books: books}))
+    fetch(API2)
+      .then(response => response.json())
+      .then(favorites => this.setState({favorites: favorites}))
   }
 
   render() {
