f854b71712c2af1c1f02f20fad6c5a77e618d7c1
rnovick13 <rnovick13@gmail.com>
2019-11-16 03:14:24 -0500

    started working on review cont

---
+++ src/components/ReviewCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ReviewCard = () => {
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default ReviewCard;
---
+++ src/containers/ReviewContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReviewCard from '../components/ReviewCard';
+
+class ReviewContainer extends Component {
+
+  fetchReviews
+
+  render() {
+    const reviewCards =
+  }
+}
+
+export default ReviewContainer
