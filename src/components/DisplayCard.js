19c40584d64630a03cbf474b8d38407200ffe146
rnovick13 <rnovick13@gmail.com>
2020-02-13 23:40:07 -0500

    book displaying title and cover

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -6,8 +6,9 @@ const DisplayCard = ({book}) => {
 
   return(
     <div>
-      <img height="100px" width="100px" src={book.cover} alt={book.title} />
-      <h3>{book.title}</h3>
+      <img height="100px" width="100px" src={book.data.attributes.cover} alt={book.data.attributes.title} />
+      <h3>{book.data.attributes.title}</h3>
+      
       <h3>Reviews</h3>
     </div>
   )
