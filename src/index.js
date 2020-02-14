7a079c0a7e3d88c01661d77726642746acfe1510
rnovick13 <rnovick13@gmail.com>
2020-02-14 02:04:04 -0500

    adding compose to index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import bookReducer from './reducers/bookReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(bookReducer)
+const store = createStore(bookReducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
