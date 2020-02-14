2bbfaccbc71f867a0329010cafa64ff411bef42a
rnovick13 <rnovick13@gmail.com>
2020-02-14 01:55:09 -0500

    worked on index file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import bookReducer from './reducers/bookReducer'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
